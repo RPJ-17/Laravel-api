fa84e48735bbf140ec23220039da2fc7fbb38338
Juan Rebolledo <juanjoserebolledo18@gmail.com>
2017-11-14 23:28:11 -0400

    products added, details also added

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\Product\Collection;
+use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
 use Illuminate\Http\Request;
 
@@ -14,7 +16,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return Collection::collection(Product::all());
     }
 
     /**
@@ -46,7 +48,8 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        //return $product;
+        return new ProductResource($product);
     }
 
     /**
