f7e05bafbabb9427ee38eae6b9e538adeefe55ae
Juan Rebolledo <juanjoserebolledo18@gmail.com>
2017-11-15 11:09:10 -0400

    added funcionality to update products

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-	protected $fillable = ['name','detail','stock','price','discount'];
+	protected $fillable = ['name','details','stock','price','discount'];
 
     public function reviews() {
     	return $this->hasMany(Review::class); 
